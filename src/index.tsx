f9f53c2662300ef7030dd9b37daf4f26c592a687
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-06-26 00:28:07 -0300

    fix: apply ident

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import './style.css';
 
 const PopoverDemo = () => (
-  <Popover.Root>
-    <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
-    <Popover.Portal>
-      <Popover.Content className="PopoverContent" sideOffset={5}>
-        Some more info…
-        <Popover.Arrow className="PopoverArrow" />
-      </Popover.Content>
-    </Popover.Portal>
-  </Popover.Root>
+    <Popover.Root>
+        <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
+        <Popover.Portal>
+            <Popover.Content className="PopoverContent" sideOffset={5}>
+                Some more info…
+                <Popover.Arrow className="PopoverArrow" />
+            </Popover.Content>
+        </Popover.Portal>
+    </Popover.Root>
 );
 
 export default PopoverDemo;
